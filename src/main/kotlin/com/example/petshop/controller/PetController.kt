39996a74949e8bb6998ea3851033bbd1eab2e352
andressamesquita <rib.mesq.andressa72@gmail.com>
2024-04-14 15:16:23 -0300

    feat: add anotação http status nos métodos cadastrar e buscar por id do controller de pet

--- src/main/kotlin/com/example/petshop/controller/PetController.kt
+++ src/main/kotlin/com/example/petshop/controller/PetController.kt
@@ -2,6 +2,7 @@ package com.example.petshop.controller
 
 import com.example.petshop.model.Pet
 import com.example.petshop.sevice.PetService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,12 +10,14 @@ import org.springframework.web.bind.annotation.*
 class PetController (val petService: PetService) {
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun cadastrar(@RequestBody pet: Pet): Pet {
         val novoPet = petService.cadastrar(pet)
         return novoPet
     }
 
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     fun buscarPorId(@PathVariable id: Long): Pet {
         val pet = petService.buscarPorId(id)
         return pet
